21fc6e16eae6af22065434f029928d15ca3845c3
noualguill <noualguill@eisti.eu>
2021-03-30 12:16:52 +0200

    Add input name

--- Front/ace-angular/src/app/app.component.ts
+++ Front/ace-angular/src/app/app.component.ts
@@ -5,6 +5,7 @@ import * as ace from "ace-builds";
 @Component({
   selector: "app-root",
   template: `
+  <input type="text" id="name">
     <div
       class="app-ace-editor"
       id="app-ace-editor"
@@ -40,15 +41,20 @@ export class AppComponent implements AfterViewInit {
   public run(){
     var e = (document.getElementById("selectLangage")) as HTMLSelectElement;
     var strLangage = e.options[e.selectedIndex].text;
-
-    
+    var input =  (document.getElementById("name")) as HTMLInputElement;
+    console.log(input.value)
     const aceEditor = ace.edit(this.editor.nativeElement);
     console.log(aceEditor.getValue())
     console.log(strLangage);
 
     const data = {
-            data : aceEditor.getValue(),
-            langage : strLangage
+      lang: strLangage,
+      files: [
+        {
+          name: input.value,
+          content: aceEditor.getValue()
+        }
+      ]
     };
     console.log(data);
     return(data);
