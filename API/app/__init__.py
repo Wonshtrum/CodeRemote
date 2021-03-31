f5cec50f8cc5aaf93fc6462a5dd7ed473480cc5b
Arthur Leclercq <arth.64@live.fr>
2021-03-31 18:46:36 +0200

    add CORS

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import *
 import pymongo
 
@@ -6,5 +7,12 @@ mongo = pymongo.MongoClient(MONGO_URL, MONGO_PORT, username=MONGO_USER, password
 db = mongo[MONGO_DATABASE]
 
 api = FastAPI()
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from app import routes
