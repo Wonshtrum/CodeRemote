7a5126af8989bf96668000686062b7e081b0fc36
Wonshtrum <pygmee64@gmail.com>
2021-03-31 22:28:36 +0200

    HTTP balancing

--- LoadBalancer/nodes.py
+++ LoadBalancer/nodes.py
@@ -1,26 +1,31 @@
 import requests as http
 from multiprocessing import Queue
 from threading import Thread
-
+from time import sleep
 
 Nodes = [
 	'127.0.0.1'
 ]
 
 
+TIMEOUT = 2
 class Network:
 	def __init__(self, nodes):
-		self.nodes = nodes
+		# (free_containers, max_time_before_free_container)
+		self.nodes = { node:None for node in nodes }
 		self.requests = Queue()
 
 	def distribute(self, request):
 		self.requests.put(request)
 	
 	def pick_node(self):
-		return self.nodes[0]
+		nodes = [ (node, state[0], state[1]) for node, state in self.nodes.items() if state is not None ]
+		if not nodes:
+			return None
+		return max(nodes, key=lambda node: (node[1], -node[2]))[0]
 
 	def send(self, request, node):
-		result = http.put(node, data=request, timeout=2)
+		result = http.put(node, data=request, timeout=TIMEOUT)
 		print(result)
 		if result.status_code != 202:
 			self.requests.put(request)
@@ -28,8 +33,20 @@ class Network:
 	def process(self):
 		while True:
 			request = self.requests.get()
-			t = Thread(target=self.send, args=(request, self.pick_node()))
+			while True:
+				node = self.pick_node()
+				if node is not None:
+					break
+				sleep(TIMEOUT)
+			t = Thread(target=self.send, args=(request, node))
 			t.start()
 	
+	def poll_node(self, node):
+		result = http.get(node, timeout=TIMEOUT)
+		print(result)
+		if result.status_code == 200:
+			status = result.json()
+			self.nodes[node] = status.
+
 	def poll(self):
-		pass
+		http.get()
