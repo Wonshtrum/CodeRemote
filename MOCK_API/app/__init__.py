c9df3d1087537ba8eeedef60f9288cd859eb92b0
Wonshtrum <pygmee64@gmail.com>
2021-03-31 18:18:15 +0200

    CORS

--- MOCK_API/app/__init__.py
+++ MOCK_API/app/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import *
 
 api = FastAPI()
+api.add_middleware(
+	CORSMiddleware,
+	allow_origins=['*'],
+	allow_methods=["*"],
+	allow_headers=["*"]
+)
 
 from app import routes
